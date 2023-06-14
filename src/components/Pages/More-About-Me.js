a021b33cfe0fb0a6faf7863d8abf89189cafc6af
Ellis Osborn <84072644+ellisosborn00@users.noreply.github.com>
2023-06-14 15:04:14 -0700

    made a More-About-Me.js

---
+++ src/components/Pages/More-About-Me.js
@@ -0,0 +1,11 @@
+import VideoSectionMoreAboutMe from "../VideoSectionMoreAboutMe";
+
+function MoreAboutMe() {
+  return (
+    <>
+      <VideoSectionMoreAboutMe />
+    </>
+  );
+}
+
+export default MoreAboutMe;
