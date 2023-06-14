337b57b984638754643e3b4e927746fd97c64963
Ellis Osborn <84072644+ellisosborn00@users.noreply.github.com>
2023-06-14 15:03:35 -0700

    added MoreAboutMe page and wanted video, so had to add a .js and .css for new video section

---
+++ src/components/VideoSectionMoreAboutMe.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../App.css";
+import "./VideoSectionMoreAboutMe.css";
+
+function VideoSectionMoreAboutMe() {
+  return (
+    <div className="video-container">
+      <video
+        src="/videos/boogie-boarding.mp4"
+        autoPlay
+        loop
+        muted
+      />
+      <h1>More About Me!</h1>
+    </div>
+  );
+}
+
+export default VideoSectionMoreAboutMe;
