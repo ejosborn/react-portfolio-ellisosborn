28572f05e6fc34ed17dc4c5ed9d04d1ec65f90d2
Ellis Osborn <84072644+ellisosborn00@users.noreply.github.com>
2023-06-15 15:12:25 -0700

    creating mission statement files and formatting them, also added the mission statement to the home page

---
+++ src/components/Home/MissionStatement.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./MissionStatement.css";
+
+function MissionStatement() {
+  return (
+    <div className="mission-statement">
+      <h1>Mission Statement</h1>
+      <div className="mission-statment__container">
+        <div className="mission-statement__wrapper">
+          <div className="mission-statement-writing">
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MissionStatement;
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,11 +1,13 @@
 import VideoSectionHome from "../Home/VideoSectionHome";
 import CardsHome from "../Home/CardsHome";
 import Footer from "../Footer";
+import MissionStatement from "../Home/MissionStatement";
 
 function Home() {
   return (
     <>
       <VideoSectionHome />
+      <MissionStatement />
       <CardsHome />
       <Footer />
     </>
