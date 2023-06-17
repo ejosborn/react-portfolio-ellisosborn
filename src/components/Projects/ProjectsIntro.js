257420d9ebe2aa91694894a6d2c90018d428782d
Ellis Osborn <84072644+ellisosborn00@users.noreply.github.com>
2023-06-17 16:12:19 -0700

    adding intro to projects

---
+++ src/components/Projects/ProjectsIntro.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./ProjectsIntro.css";
+
+function ProjectsIntro() {
+  return (
+    <div className="projects-intro">
+      <div className="projects-intro-container">
+        <div className="projects-intro__wrapper">
+          <div className="projects-intro__writing">
+            <h1>Languages I Have Used</h1>
+            <p>
+              <br />
+              C#, C++, Java, SQL, PostgreSQL, Python, C,
+              <br />
+            </p>
+
+            <h1>
+              <br />
+              Frameworks
+              <br />
+            </h1>
+            <p>
+              <br /> React, Xamarin,
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectsIntro;
