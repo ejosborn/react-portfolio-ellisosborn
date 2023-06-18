c37f7348104bfb4aedcc8f7ae731e2a5c8ccd692
Ellis Osborn <84072644+ellisosborn00@users.noreply.github.com>
2023-06-17 18:39:16 -0700

    added cards for users to see projects I have worked on in the past

--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -1,12 +1,14 @@
 import VideoSectionProjects from "../Projects/VideoSectionProjects";
 import ProjectsIntro from "../Projects/ProjectsIntro";
 import Footer from "../Footer";
+import CardsProject from "../Projects/CardsProject";
 
 export default function Projects() {
   return (
     <>
       <VideoSectionProjects />
       <ProjectsIntro />
+      <CardsProject />
       <Footer />
     </>
   );
---
+++ src/components/Projects/CardsProject.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./CardsProject.css";
+import CardItem from "../CardItem";
+
+function CardsProject() {
+  return (
+    <div className="cards">
+      <h1>Check out some projects I have worked on</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src="images/img-capstone.jpg"
+              text="Windows App Developement for UAV Fuel Cell System"
+              path="/Projects/Project1"
+            />
+
+            <CardItem
+              src="images/img-instant-messaging-app.jpg"
+              text="Instant Messaging Application"
+              path="/Projects/Project2"
+            />
+
+            <CardItem
+              src="images/img-sudoku.jpg"
+              text="Sudoku Solver"
+              path="/Projects/Project1"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardsProject;
