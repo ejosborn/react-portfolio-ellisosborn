0823180b883115fa029264f3d0110c84c7fdbd3e
Ellis Osborn <ejosborn@asu.edu>
2023-06-28 00:35:42 -0700

    removed button

--- src/components/Home/VideoSectionHome.js
+++ src/components/Home/VideoSectionHome.js
@@ -1,23 +1,18 @@
 import React from "react";
 import "../../App.css";
-import { Button } from "../Button";
 import "./VideoSectionHome.css";
 
 function VideoSection() {
   return (
     <div className="video-container">
-      <video src="/videos/nav-video1.mp4" autoPlay loop muted />
+      <video
+        src="/videos/nav-video1.mp4"
+        autoPlay
+        loop
+        muted
+      />
       <h1>Welcome to The Website</h1>
       <p>Ellis Osborn</p>
-      <div className="hero-btns">
-        <Button
-          className="btns"
-          buttonStyle="btn--primary"
-          buttonSize="btn--large"
-        >
-          About Me! <i class="fa-light fa-user-astronaut"></i>
-        </Button>
-      </div>
     </div>
   );
 }
