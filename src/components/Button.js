52d56a91cfdbddf50c8698a6f305e86a37460a24
Ellis Osborn <84072644+ellisosborn00@users.noreply.github.com>
2023-06-27 20:20:34 -0700

    added style of button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Button.css";
 
-const STYLES = ["btn--primary", "btn--outline", "btn--test"];
+const STYLES = [
+  "btn--primary",
+  "btn--outline",
+  "btn--secondary",
+];
 const SIZES = ["btn--medium", "btn--large"];
 
 export const Button = ({
@@ -21,7 +25,7 @@ export const Button = ({
     : SIZES[0];
 
   return (
-    <Link to="/About-Me" className="btn-mobile">
+    <Link to="/Projects" className="btn-mobile">
       <button
         className={`btn ${checkButtonStyle} ${checkButtonSize}`}
         onClick={onClick}
