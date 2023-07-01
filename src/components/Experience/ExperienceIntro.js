b229c4929a825c8c5cb42790b5b14dc322e792a4
Ellis Osborn <ejosborn@asu.edu>
2023-07-01 01:07:23 -0700

    added content to experience, education, and interests section

--- src/components/Experience/ExperienceIntro.js
+++ src/components/Experience/ExperienceIntro.js
@@ -6,28 +6,128 @@ function ExperienceIntro() {
   return (
     <div className="experience-intro">
       <div className="experience-intro__bio">
-        <h1>About Me</h1>
+        <h1>Experience</h1>
         <div className="experience-intro__container">
           <div className="experience-intro__wrapper">
             <div className="experience-intro__content">
               <div className="experience-intro__bio-experience">
                 <h2>Experience</h2>
-                <p>Something</p>
+                {/* Experience 1 */}
+                <h5>
+                  Army Reserves(MOS: 35S) Signals Collection
+                  Analyst
+                </h5>
+                <p>November 2017 - Present</p>
+                <p>
+                  &nbsp;&nbsp;&nbsp;- Top Secret Clearance -
+                  TS/SCI
+                </p>
+                <p>
+                  &nbsp;&nbsp;&nbsp;- Responsible for
+                  detection, acquisition, location and
+                  identification of foreign electronic
+                  intelligence
+                </p>
+                <p>
+                  &nbsp;&nbsp;&nbsp;- Trained in exploiting
+                  non-voice communications and other
+                  electronic signals to provide
+                  strategic/tactical intelligence
+                </p>
+                <br />
+
+                {/* Experience 2 */}
+                <h5>
+                  Wave Plumbing -- IT and Online Sales
+                  Coordinator
+                </h5>
+                <p>January 2020 - Present</p>
+                <p>
+                  &nbsp;&nbsp;&nbsp;- Troubleshoot software
+                  and structural technological issues for a
+                  private business
+                </p>
+                <p>
+                  &nbsp;&nbsp;&nbsp;- Perform computer
+                  literacy education to employees
+                </p>
+                <p>
+                  &nbsp;&nbsp;&nbsp;- Implemented online
+                  vendor and social media presence
+                </p>
+                <p>
+                  &nbsp;&nbsp;&nbsp;- Facilitated the
+                  transition to online platforms during the
+                  COVID-19 pandemic
+                </p>
+                <br />
+
+                {/* Experience 3 */}
+                <h5>Best Buy -- Sales Consultant</h5>
+                <p>August 2022 - Present</p>
+                <p>
+                  &nbsp;&nbsp;&nbsp;- Consult with customers
+                  to find best technology solutions for
+                  their needs
+                </p>
+                <p>
+                  &nbsp;&nbsp;&nbsp;- Responsible for sales
+                  in computer-based products department and
+                  home theater/home audio department
+                </p>
+                <p>
+                  &nbsp;&nbsp;&nbsp;- Develop, maintain, and
+                  communicate strong, up-to-date knowledge
+                  of products and accessories
+                </p>
+                <br />
               </div>
               <div className="experience-intro__bio-education">
                 <h2>Education</h2>
                 <p>
-                  I received a BS in Computer Science with a
-                  concentration in Software Engineering
+                  I received a B.S in Computer Science with
+                  a concentration in Software Engineering.
+                  Throughout my education at Arizona State
+                  University, I learned the OOP languages
+                  such as: Java, Python, C++, and C. I
+                  learned data structures and algorithms.
+                  Critically thinking to come up with
+                  solutions has been one of the most
+                  prominent skills that I have learned
+                  throughout my education.
+                  <br />
+                  <br />
+                  With my concentration in Software
+                  Engineering, I expanded my knowledge
+                  through specialized coursework such as
+                  database management and development,
+                  service-oriented computing, software
+                  quality assurance testing, software
+                  analysis, design, and architecture, and
+                  the product lifecycle.
                 </p>
+                <br />
               </div>
               <div className="experience-intro__bio-interests">
                 <h2>Interests</h2>
-                <p>I am interested in</p>
+                <p>
+                  I am interested in joining the field of
+                  aerospace as an entry level or associate
+                  software engineer, but I welcome all other
+                  opportunities. Some other opportunities
+                  I'd be interested in exploring are testing
+                  code and creating documentation, front end
+                  and back end development, web development,
+                  and database administration.
+                </p>
+                <br />
               </div>
             </div>
             <div className="experience-intro__resume">
-              <a href="/Resume.pdf">See My Resume</a>
+              <a href="/Resume.pdf" target="_blank">
+                See My Resume
+              </a>
+              <br />
               <br />
               <Button
                 className="btns"
